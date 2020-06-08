2bdd36d85974844af7c8cde27e06680de9775529
neel526 <neelm526@gmail.com>
2020-06-08 13:17:11 +0530

    initial commit

---
+++ src/core/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../styles.css";
+import { API } from '../backend';
+import Base from './Base';
+
+
+
+export default function Home() {
+    console.log("API IS", API);
+
+    return (
+       <Base title="Home Page" description="Welcome to the Learnway ">
+           
+         </Base>
+    )
+}
